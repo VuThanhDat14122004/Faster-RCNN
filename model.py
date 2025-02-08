a0bf68368d385b9639602d220f44b7755a48203c
unknown <vuthanhdat019@gmail.com>
2025-02-08 10:54:03 +0700

    make proposal and region proposal network

--- model.py
+++ model.py
@@ -4,5 +4,20 @@ from torchvision import ops
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn as nn
+from torchvision.models import vgg16, VGG16_Weights
 
 from utils import *
+
+class FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+        model = vgg16(weights = VGG16_Weights)
+        required_layers = list(model.children())[0][:17]
+        self.backbone = nn.Sequential(*required_layers)
+        for param in self.backbone.parameters():
+            param.requires_grad = True
+    
+    def forward(self, x):
+        return self.backbone(x)
+
+
